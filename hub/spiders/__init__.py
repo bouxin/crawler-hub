8285f9bd74a2b6cd1a0ef92ef28b874d49818f7c
lubosson <mksense.happy@gmail.com>
2020-10-02 22:08:39 +0800

    spiders

---
+++ hub/spiders/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @author lubo
+# @since 2020-10-02
+#
+# This package will contain the spiders of your Scrapy project
+# Please refer to the documentation for information on how to create and manage
+# your spiders.
+
---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @author lubo
+# @since 2020-10-02
+# @description app main
+
+
+from scrapy.cmdline import execute
+execute(['scrapy', 'crawl', 'hub'])
